e7354c1a2d862c16c71e2c43b7baeaa463f23850
David <136182039+david0154@users.noreply.github.com>
2026-01-09 16:54:38 +0530

    Fix: Correct Hilt plugin ID to com.google.dagger.hilt.android

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "8.1.0" apply false
     kotlin("android") version "1.9.0" apply false
     kotlin("kapt") version "1.9.0" apply false
-    id("dagger.hilt.android.plugin") version "2.46" apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
 }
 
