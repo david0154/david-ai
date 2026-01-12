2410fc91c1272f31898311bfaea5ce2a5eb79a96
David <136182039+david0154@users.noreply.github.com>
2026-01-12 09:19:30 +0530

    Update AGP to 8.7.3 to fix SDK XML version 4 compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.29" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 tasks.register("clean", Delete::class) {
